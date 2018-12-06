7265ccb52c559978db41826f885acf0de55273b7
Ni4991 <geraldthepie@yahoo.com>
2018-12-06 01:21:02 -0800

    add delete

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,4 +10,10 @@ module.exports = (app) => {
   app.post('/api/todos/:todoId/items', todoItemsController.create);
   app.get('/api/todos/:todoId', todosController.retrieve);
   app.put('/api/todos/:todoId', todosController.update);
+  app.delete('/api/todos/:todoId', todosController.destroy);
+  
+  app.all('/api/todos/:todoId/items', (req, res) =>
+  res.status(405).send({
+    message: 'Method Not Allowed',
+}));
 };
