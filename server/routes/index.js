59ab28624858511be225d434636812320c43cb42
Ni4991 <geraldthepie@yahoo.com>
2018-12-06 01:16:29 -0800

    add update todo

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,8 +5,9 @@ module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
     message: 'Welcome to the Todos API!',
   }));
-
   app.get('/api/todos', todosController.list);
   app.post('/api/todos', todosController.create);
   app.post('/api/todos/:todoId/items', todoItemsController.create);
+  app.get('/api/todos/:todoId', todosController.retrieve);
+  app.put('/api/todos/:todoId', todosController.update);
 };
